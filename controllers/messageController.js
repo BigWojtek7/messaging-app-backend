66e734f049e590f1df5fd63dd92f6e7955520b8a
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-07-05 07:58:04 +0200

    Change names of controllers

---
+++ controllers/messageController.js
@@ -0,0 +1,44 @@
+// const User = require('../models/user');
+// const bcrypt = require('bcryptjs');
+const Message = require('../models/messages');
+const { body, validationResult } = require('express-validator');
+
+exports.all_messages_get = async (req, res) => {
+  try {
+    const allUserMessages = await Message.find({ receiver: req.params.userId })
+      .sort({ date: 1 })
+      .populate('user')
+      .exec();
+    res.json({ messages: allUserMessages });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.create_message_post = [
+  body('title', 'title is require').trim().isLength({ min: 1 }).escape(),
+  body('content', 'content is required').trim().isLength({ min: 1 }).escape(),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    const message = new Message({
+      title: req.body.title,
+      content: req.body.content,
+      date: new Date(),
+      author: req.params.id,
+      receiver: req.body.receiver,
+    });
+
+    if (!errors.isEmpty()) {
+      res.json({ success: false, msg: errors.array() });
+    } else {
+      try {
+        await message.save();
+      } catch (err) {
+        console.log(err);
+      }
+      res.json({ success: true, msg: 'Message has been sent' });
+    }
+  },
+];
---
+++ controllers/userController.js
@@ -0,0 +1,57 @@
+const passport = require('passport');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+exports.login_post = passport.authenticate('local');
+
+exports.authenticate_get = (req, res) => {
+  const isAuthenticated = req.isAuthenticated();
+  res.json({ authenticated: isAuthenticated });
+};
+
+exports.register_post = [
+  body('username', 'Username is required').trim().isLength({ min: 1 }).escape(),
+  body('password', 'Password is required').trim().isLength({ min: 1 }).escape(),
+  body('re_password', 'Passwords do not match')
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .trim()
+    .escape(),
+
+  async (req, res) => {
+    const userInDatabase = await User.find({
+      username: req.body.username,
+    }).exec();
+
+    if (userInDatabase.length > 0) {
+      res.json({ success: false, msg: 'Username already exists' });
+      return;
+    }
+
+    const errors = validationResult(req);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+
+    if (!errors.isEmpty()) {
+      res.json({ success: false, msg: errors.array() });
+    } else {
+      await user.save();
+      res.json({ success: true, msg: 'New user has been saved' });
+    }
+  },
+];
+
+exports.logout_get = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+};
