2665b805eca99029238a004f933215e55bfa9876
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-07-18 17:50:33 +0200

    Add passport.authenticate to endpoints

--- routes/messages.js
+++ routes/messages.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const message_controller = require('../controllers/messageController');
 
 /* GET users listing. */
-router.get('/:userid/messages-num', message_controller.message_count_get);
-router.get('/:userid/messages', message_controller.all_messages_get);
+router.get(
+  '/:userid/messages-num',
+  passport.authenticate('jwt', { session: false }),
+  message_controller.message_count_get
+);
+router.get(
+  '/:userid/messages',
+  passport.authenticate('jwt', { session: false }),
+  message_controller.all_messages_get
+);
 
-router.post('/create-message', message_controller.create_message_post);
-router.delete('/:messageid/delete', message_controller.message_delete)
+router.post(
+  '/create-message',
+  passport.authenticate('jwt', { session: false }),
+  message_controller.create_message_post
+);
+router.delete(
+  '/:messageid/delete',
+  passport.authenticate('jwt', { session: false }),
+  message_controller.message_delete
+);
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const user_controller = require('../controllers/userController');
-router.get('/user', user_controller.user_get);
-router.get('/all-users', user_controller.all_usernames_get)
+router.get(
+  '/user',
+  passport.authenticate('jwt', { session: false }),
+  user_controller.user_get
+);
+router.get(
+  '/all-users',
+  passport.authenticate('jwt', { session: false }),
+  user_controller.all_usernames_get
+);
 router.post('/login', user_controller.user_login_post);
-router.patch('/:userid/username', user_controller.username_edit)
-router.patch('/:userid/password', user_controller.password_edit)
+router.patch(
+  '/:userid/username',
+  passport.authenticate('jwt', { session: false }),
+  user_controller.username_edit
+);
+router.patch(
+  '/:userid/password',
+  passport.authenticate('jwt', { session: false }),
+  user_controller.password_edit
+);
 
 // router.get('/login-failure', user_controller.login_failure_get)
 
-
 router.post('/register', user_controller.register_post);
-
 
 router.get('/logout', user_controller.logout_get);
 
